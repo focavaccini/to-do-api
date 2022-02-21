727d4fdee637f4d62f159b7a4069304fc85a1919
Fernando Cavaccini <fernandocavaccini@gmail.com>
2022-02-21 17:58:54 -0300

    Task class created and mapped

---
+++ src/main/java/com/todoapi/entities/Task.java
@@ -0,0 +1,62 @@
+package com.todoapi.entities;
+
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Task {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	
+	public Task() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Task(Long id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Task other = (Task) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+}
