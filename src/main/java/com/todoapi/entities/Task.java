fbe90321c04d41a010b2e5cb5ba2e86fb50fc1dc
Fernando Cavaccini <fernandocavaccini@gmail.com>
2022-02-23 14:29:19 -0300

    Database change to PostgreSQL

--- src/main/java/com/todoapi/entities/Task.java
+++ src/main/java/com/todoapi/entities/Task.java
@@ -9,12 +9,14 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.todoapi.entities.enums.PriorityTask;
 import com.todoapi.entities.enums.StatusTask;
 
 @Entity
+@Table(name = "tb_task")
 public class Task implements Serializable{
 
 	private static final long serialVersionUID = 1L;
